f60721762cba929e2dffbf3839e7abac71612497
Geir Damstuen <geirda@gmail.com>
2019-09-10 08:51:24 +0200

    Add files

--- .dwm/statusbar.sh
+++ .dwm/statusbar.sh
@@ -3,7 +3,7 @@
 do
 	RAM="$(free -m | awk '/Mem/{ print $3 }')MB"
 	DISKUSE="$(df -h | awk '/sda1/{ print $4 }')B"
-	IP=$(ip addr show dev enp0s3 | awk '/inet /{ print $2 }')
+	IP=$(ip addr | awk '/ global /{ print $2 }')
 	DATO=$(date '+%a %d %b %H:%M')
 	xsetroot -name "Ram use: $RAM | Free space: $DISKUSE | IP: $IP | $DATO"
 	sleep 5;

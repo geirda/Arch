ca165a7733fa2168360cac4df993dfb59cb5bcab
Geir Damstuen <geirda@gmail.com>
2019-11-07 08:23:01 +0100

    Small changes

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-setxkbmap no
 compton --config $HOME/.config/compton.conf -b &
 feh --bg-scale $HOME/Bilder/wallpapers/Arch-linux-1920x1080.jpg &
 guake &
-/home/geir/.dwm/statusbar.sh
+$HOME/.dwm/statusbar.sh

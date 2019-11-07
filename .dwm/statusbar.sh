0fd1ca2aa939412af4000d957758e420bb471009
Geir Damstuen <geirda@gmail.com>
2019-11-07 07:56:04 +0100

    Added icons

--- .dwm/statusbar.sh
+++ .dwm/statusbar.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
-while true;
+ while true;
 do
+	UPDATES="$(pacman -Qu|wc -l)"
 	RAM="$(free -m | awk '/Mem/{ print $3 }')MB"
 	DISKUSE="$(df -h | awk '/sda1/{ print $4 }')B"
 	IP=$(ip addr | awk '/ global /{ print $2 }')
 	DATO=$(date '+%a %d %b %H:%M')
-	xsetroot -name "Ram use: $RAM | Free space: $DISKUSE | IP: $IP | $DATO"
+	xsetroot -name " Updates: $UPDATES |  Ram use: $RAM |  Free space: $DISKUSE |  IP: $IP |  $DATO"
 	sleep 5;
 done;

6b2af737e26c1bd282d235613cc51d1cc61994e8
Geir Damstuen <geir@gmail.com>
2020-08-04 09:10:01 +0200

    Small changes

--- .config/dwm/statusbar-color.sh
+++ .config/dwm/statusbar-color.sh
@@ -17,10 +17,11 @@ do
 	HEAT="$(sensors | awk '/Core 0/{ print $3 }') $(sensors | awk '/Core 1/{ print $3 }')"
 	DISKUSE="$(df -h | awk '/sda2/{ print $4 }')B"
 	IP=$(ip addr | awk '/ global /{ print $2 }'|head -1)
+	WEATHER=$(~/.local/bin/weather.sh)
 	DATO=$(date '+uke %V:^c#b48ead^ %a%d')
 	MONTH=$(date '+%b'|cut -b1-3)
+	KLOKKEIKON=$(~/.local/bin/hour.sh)
 	KLOKKE=$(date '+%H:%M')
-	WEATHER=$(/home/geir/.local/bin/weather.sh)
-	xsetroot -name " ${GREEN}updates: $UPDATES ${GRAY}|^d^  ${BLUE}mem: $RAM ${GRAY}|^d^  ${RED}$HEAT ${GRAY}|^d^  ${CYAN}free: $DISKUSE ${GRAY}|^d^  ${YELLOW}$IP ${GRAY}|^d^ ${ORANGE} ${WHITE}$WEATHER ${GRAY}|^d^  ${GREEN}$DATO.$MONTH ^d^ ${BLUE}$KLOKKE "
+	xsetroot -name " ${GREEN}updates: $UPDATES ${GRAY}|^d^  ${BLUE}mem: $RAM ${GRAY}|^d^  ${RED}$HEAT ${GRAY}|^d^  ${CYAN}free: $DISKUSE ${GRAY}|^d^  ${YELLOW}$IP ${GRAY}|^d^ ${WHITE}$WEATHER ${GRAY}|^d^  ${GREEN}$DATO.$MONTH ^d^$KLOKKEIKON ${BLUE}$KLOKKE "
 	sleep 5;
 done;

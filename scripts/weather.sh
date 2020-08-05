bb85231de7f7d23553b1e1b91392ffa062f01d45
Geir Damstuen <geir@gmail.com>
2020-08-05 09:55:44 +0200

    Add scripts

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,55 +1,76 @@
 #!/bin/sh
-[ -f ~/.cache/yr/data.txt ] && WEATHER="$(head -1 ~/.cache/yr/data.txt)C" || WEATHER="X°C"
-CONDITIONS="$(head -1 ~/.cache/yr/data2.txt | cut -f1 -d .)"
+
+STI="~/.cache/yr"
+
+[ -f "$STI/data.txt" ] && WEATHER="$(head -1 $STI/data.txt)C" || WEATHER="X°C"
+[ -f "$STI/varsel.xml" ] && CONDITIONS="$(grep enclosure $STI/varsel.xml | head -1 | cut -f2 -d \"| cut -f9 -d \/)" || IKON="*"
 
 case "$CONDITIONS" in
-Skyet*)
-	IKON=" "
+01d.png)
+	IKON=" "
 	;;
-Overskyet*)
-	IKON=" "
+01n.png)
+	IKON=" "
 	;;
-Lettskyet*)
+02d.png)
 	IKON=" "
 	;;
-Delvis*)
+02n.png)
+	IKON=" "
+	;;
+03d.png)
 	IKON=" "
 	;;
-Regn*)
+03n.png)
+	IKON=" "
+	;;
+04.png)
+	IKON=" "
+	;;
+05d.png)
+	IKON=" "
+	;;
+05n.png)
+	IKON=" "
+	;;
+06d.png)
+	IKON=" "
+	;;
+06n.png)
+	IKON=" "
+	;;
+07d.png)
+	IKON=" "
+	;;
+07n.png)
+	IKON=" "
+	;;
+08d.png)
+	IKON=" "
+	;;
+08n.png)
+	IKON=" "
+	;;
+09.png)
 	IKON=" "
 	;;
-Kraftig*regn*)
+10.png)
 	IKON=" "
 	;;
-*regn*)
-	IKON=" "
-	;;
-Snø*)
-	IKON=" "
-	;;
-*snø*)
-	IKON=" "
-	;;
-Sludd*)
-	IKON=" "
-	;;
-*sludd*)
-	IKON=" "
-	;;
-Hagl*)
-	IKON=" "
-	;;
-*hagl*)
-	IKON=" "
-	;;
-Torden*)
+11.png)
 	IKON=" "
 	;;
-*torden*)
-	IKON=" "
+12.png)
+	IKON=" "
 	;;
-*)
-	IKON=" "
+13.png)
+	IKON=" "
+	;;
+14.png)
+	IKON=" "
+	;;
+15.png)
+	IKON=" "
 	;;
 esac
 

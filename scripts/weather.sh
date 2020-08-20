0c780a9b689b4e05804cc29d8d13cc2c7716535a
Geir Damstuen <geir@gmail.com>
2020-08-20 16:46:38 +0200

    Add more weather icons

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -29,6 +29,46 @@ case "$CONDITIONS" in
 	13.png)		IKON=" "	;;
 	14.png)		IKON=" "	;;
 	15.png)		IKON=" "	;;
+	20d.png)	IKON=" "	;;
+	20n.png)	IKON=" "	;;
+	21d.png)	IKON=" "	;;
+	21n.png)	IKON=" "	;;
+	22.png)		IKON=" "	;;
+	23.png)		IKON=" "	;;
+	24d.png)	IKON=" "	;;
+	24n.png)	IKON=" "	;;
+	25d.png)	IKON=" "	;;
+	25n.png)	IKON=" "	;;
+	26d.png)	IKON=" "	;;
+	26n.png)	IKON=" "	;;
+	27d.png)	IKON=" "	;;
+	27n.png)	IKON=" "	;;
+	28d.png)	IKON=" "	;;
+	28n.png)	IKON=" "	;;
+	29d.png)	IKON=" "	;;
+	29n.png)	IKON=" "	;;
+	30.png)		IKON=" "	;;
+	31.png)		IKON=" "	;;
+	32.png)		IKON=" "	;;
+	33.png)		IKON=" "	;;
+	34.png)		IKON=" "	;;
+	40d.png)	IKON=" "	;;
+	40n.png)	IKON=" "	;;
+	41d.png)	IKON=" "	;;
+	41n.png)	IKON=" "	;;
+	42d.png)	IKON=" "	;;
+	42n.png)	IKON=" "	;;
+	43d.png)	IKON=" "	;;
+	43n.png)	IKON=" "	;;
+	44d.png)	IKON=" "	;;
+	44n.png)	IKON=" "	;;
+	45d.png)	IKON=" "	;;
+	45n.png)	IKON=" "	;;
+	46.png)		IKON=" "	;;
+	47.png)		IKON=" "	;;
+	48.png)		IKON=" "	;;
+	49.png)		IKON=" "	;;
+	50.png)		IKON=" "	;;
 esac
 
 echo $IKON $WEATHER

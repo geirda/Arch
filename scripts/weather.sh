752809a228cce0e0d1f52cf9bb7371c5e8966170
Geir Damstuen <geir@gmail.com>
2020-08-23 11:04:38 +0200

    Some icon changes

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This script requires a patched nerd font for the weather icons
 
-STI="$HOME/.cache/yr"
+STI="$HOME/Ubuntu/.cache/yr"
 
 [ -f "$STI/data.txt" ] && WEATHER="$(head -1 $STI/data.txt)C" || WEATHER="X°C"
 [ -f "$STI/varsel.xml" ] && CONDITIONS="$(grep enclosure $STI/varsel.xml | head -1 | cut -f2 -d \"| cut -f9 -d \/)" || IKON="*"
@@ -15,13 +15,13 @@ case "$CONDITIONS" in
 	03n.png)	IKON=" "	;;
 	04.png)		IKON=" "	;;
 	05d.png)	IKON=" "	;;
-	05n.png)	IKON=" "	;;
+	05n.png)	IKON=" "	;;
 	06d.png)	IKON=" "	;;
-	06n.png)	IKON=" "	;;
+	06n.png)	IKON=" "	;;
 	07d.png)	IKON=" "	;;
-	07n.png)	IKON=" "	;;
+	07n.png)	IKON=" "	;;
 	08d.png)	IKON=" "	;;
-	08n.png)	IKON=" "	;;
+	08n.png)	IKON=" "	;;
 	09.png)		IKON=" "	;;
 	10.png)		IKON=" "	;;
 	11.png)		IKON=" "	;;

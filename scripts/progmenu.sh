a792a310eb83b45a39effbba788c39765cd8137a
Geir Damstuen <geir@gmail.com>
2020-09-14 08:32:34 +0200

    Add dmenu script for a short selection of apps/actions

--- scripts/progmenu.sh
+++ scripts/progmenu.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-prog="Pcmanfm
-Firefox
-Brave
-LOWriter
-Gimp
+prog="LOWriter
 KeepassXC
 VirtualBox
-Alacritty
-St
 Appearance
 Wallpaper
 Shutdown
@@ -17,15 +11,9 @@ Reboot"
 cmd=$(dmenu -h 24 -l 15 -nb '#0a0a0a' -nf '#808080' -sb '#3a3436' -sf '#cac5b5' -p 'Launcher' <<< "$prog")
 
 case ${cmd%% *} in
-	Pcmanfm)	pcmanfm ;;
-	Firefox)	firefox ;;
-	Brave)		brave ;;
 	LOWriter)	lowriter ;;
-	Gimp)		gimp ;;
 	KeepassXC)	keepassxc ;;
 	VirtualBox)	virtualbox ;;
-	Alacritty)	alacritty ;;
-	St)		st ;;
 	Appearance)	lxappearance ;;
 	Wallpaper)	nitrogen ;;
 	Shutdown)	poweroff ;;

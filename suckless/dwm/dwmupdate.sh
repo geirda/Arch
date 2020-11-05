536d52cd99f2f1d9fb01bc32c6ddc6aa6bed65cb
Geir Damstuen <geir@gmail.com>
2020-11-05 17:52:18 +0100

    Add update script for dwm

--- suckless/dwm/dwmupdate.sh
+++ suckless/dwm/dwmupdate.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 /usr/bin/echo -en "Update script for dwm-geir\n\n"
-wget -q https://raw.githubusercontent.com/geirda/Arch/master/suckless/dwm/README.md -O /tmp/dwm-geir
 
-NYESTE=$(grep pacman /tmp/dwm-geir | cut -f5- -d " "| cut -f3-4 -d "-")
 INSTALLERT=$(pacman -Qs dwm-geir|head -1|awk '{print $2}')
 
 [ -z $INSTALLERT ] && echo "dwm-geir is not installed" || echo "Installed version: $INSTALLERT"
 
-[ "$NYESTE" = "$INSTALLERT" ] && echo "You are on the latest version" && exit 0
+wget -q https://raw.githubusercontent.com/geirda/Arch/master/suckless/dwm/README.md -O /tmp/dwm-geir
+NYESTE=$(grep pacman /tmp/dwm-geir | cut -f5- -d " "| cut -f3-4 -d "-")
+
+[ "$NYESTE" = "$INSTALLERT" ] && echo "You have the latest version" && exit 0
 
 echo -n "Latest version is $NYESTE. Do you want to install it? [y/N] "
 read SVAR

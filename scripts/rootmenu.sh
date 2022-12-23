e46a3fa5eeba761329738c521deaf424b9d53c86
Geir Damstuen <geir@gmail.com>
2022-12-23 19:56:23 +0100

    Small adjustments

--- scripts/rootmenu.sh
+++ scripts/rootmenu.sh
@@ -6,11 +6,15 @@ IMG:$ICONPATH/apps/nautilus.png			File manager (PCManFM)		pcmanfm
 
 IMG:$ICONPATH/apps/firefox.png			Web browser (firefox)		ffoxrun.sh
 IMG:$ICONPATH/apps/brave.png			Web browser (brave)		brave
+
 IMG:$ICONPATH/apps/libreoffice-writer.png	Word processor (lowriter)	lowriter
 IMG:$ICONPATH/apps/gimp.png			Image manipulation (gimp)	gimp
+IMG:$ICONPATH/apps/audacious.png		Music player (audacious)	audacious
 
 IMG:$ICONPATH/apps/aisleriot.png		Solitaire (aisleriot)		sol
 IMG:$ICONPATH/apps/gnome-mahjongg.png		Mahjongg			gnome-mahjongg
+IMG:$ICONPATH/apps/gnome-games.png		Minesweeper			gnome-mines
+IMG:$ICONPATH/apps/wps-spreadsheets.png		Sudoku				gnome-sudoku
 
 IMG:$ICONPATH/apps/keepassxc.png		Password manager (KeePass)	keepassxc
 IMG:$ICONPATH/apps/virtualbox.png		Virtualbox			virtualbox
